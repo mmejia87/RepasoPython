7b0072688910e72b6a8101cf1a3a00e1c9c7cf19
mmejia87 <manuelmejia_rdz@hotmail.com>
2023-06-22 18:06:12 -0600

    Repaso01

---
+++ Repaso/01.py
@@ -0,0 +1,190 @@
+
+# INPUT/PRINT
+
+# print('Tu nombre es ' + input('Dime tu nombre: ') + " " + input('Dime tu apellido: '))
+# print('Tu estudias: ' + input('Que estas estudiando: '))
+# print('Vives en '+ input('En que pais vives: '))
+# print('Tu nombre completo es: ' + input('Escribe tu nombre: ') +" "+ input('Escribe tu apellido: '))
+
+###############################################################################################################
+
+#Dia 02
+
+#VARIABLES
+
+# nombre = 'Coshy'
+# print(nombre)
+
+# edad = 45
+# print(edad)
+
+# edad2 = 10
+
+# print(edad+edad2)
+
+# nombre = input('Cual es tu nombre: ')
+
+# print('Tu Name is ' + nombre)
+
+# nombre = "hello "
+# nombre2 = 'Coshy'
+# frase = nombre + nombre2
+# print(frase) 
+
+# num = 5
+# num1 = 10
+
+# operacion = num + num1
+# print(operacion)
+
+# nombre = 'Tony Soprano'
+# edad = ' 51'
+
+# print(nombre + edad)
+
+# nombre = 'Julia '
+# apellido = 'Roberts'
+ 
+# nombrecompleto = nombre + apellido
+
+# print(nombrecompleto)
+
+# curso = 'Python'
+# print('Estas tomando un curso de ' + curso)
+
+# mi_numero = 5 + 5
+# print(mi_numero + mi_numero )
+
+# print(type(mi_numero))
+
+# mi_numero = 1.5 + 1.5
+# print(mi_numero * mi_numero)
+# print(type(mi_numero))
+
+# edad = input("escribe tu edad: ")
+# print('Tu edad es '+ edad)
+# print(type(edad))
+
+# num_entero = 5
+# print(type(num_entero))
+# print(num_entero)
+
+# num1 = 7.5
+# num2 = 2.5
+# num3 = num1 + num2
+# print(num3)
+# print(type(num3))
+
+# CONVERSIONES
+# CONVERSIONES IMPLISITAS
+
+# num1 = 20
+# num2 = 30.5
+
+# print(type(num1))
+# print(type(num2))
+
+# num1 = 20 
+# num2 = 30.5
+
+# num1 = num1 + num2
+# print(type(num1))
+# print(type(num2))
+
+# CONVERSIONES EXPLICITAS
+
+# num1 = 5.8
+# print(num1)
+# print(type(num1))
+
+# num2 = int(num1)
+# print(num2)
+# print(type(num2))
+
+# edad = input('Dime tu edad: ')
+# print(type(edad))
+# edad = int(edad)
+# print(type(edad))
+# nueva_edad = 1 + edad
+# print(nueva_edad)
+
+# num1 = 7.5
+# print(type(num1))
+
+# num2 = int(num1)
+# print(num2)
+# print(type(num2))
+
+# num2 = 10
+# print(type(num2))
+
+# num1 = float(num2)
+# print(type(num1))
+
+# OPERACIONES MATEMATICAS
+
+# x = 6
+# y = 4
+# z = 7
+
+# # print(f"{x} mas {y} es igual a {x + y}")
+# # print(f"{x} menos {y} es igual a {x - y}")
+# # print(f"{x} por {y} es igual a {x * y}")
+# # print(f"{x} entre {y} es igual a {x / y}")
+
+# # Division al piso
+
+# print(f"{z} divido al piso de {y} es igual a {z//y}")
+# print(f"{z} modulo de {y} es igual {z%y}")
+# print(f"{x} elevado a la {y} es igual a {x**y}")  #POTENCIA
+# print(f"{x} elevado a la {3} es igual a {x**3}")   #Cubo
+# print(f"La raiz cuadrada de {x} es {x**0.5}")  #raiz cuadrada
+
+# x = 874
+# y = 27
+
+# print(f"{x} modulo de {y} es igual {x%y}")
+
+
+# REDONDEAR
+
+# print(90/7)
+# print(round(90/7))
+
+# resultado = 90 / 7
+# redondeo = round(resultado) 
+# print(resultado)
+
+# valor = round(95.66666666,2)
+# valor = round(95.66666666,3)
+# valor = round(95.66666666,4)
+# print(valor)
+
+# valor = round(95.6666666666666)
+# print(valor)
+# print(type(valor))
+
+# valor = 95.666666666
+# print(round(valor))
+# print(type(valor))
+
+# resul = round (10/3 , 2)
+# print(resul)
+############################################################################
+# PROYECTO 2
+
+# CALCULAR COMSIONES
+
+# nombre = input("por favor dime tu nombre: ")
+# ventas = int(input("ventas totales del mes: "))
+# # ventas = input("ventas totales del mes: ")
+# # ventas = int(ventas)
+# comision = round(ventas * 13 / 100, 2)
+# # comision = ventas * 13 /100
+# # comision = round(comision,2)
+
+# print(f"Hola {nombre}, tus comisiones de este mes son de ${comision}")
+
+############################################################################
+
+# Dia 03
