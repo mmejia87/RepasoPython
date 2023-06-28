ab603fd2e4a0ac701c7429ee1ef334baacd16a6e
Manu <109192697+mmejia87@users.noreply.github.com>
2023-06-28 14:39:34 -0600

    Add files via upload

---
+++ dia01.py
@@ -0,0 +1,48 @@
+# FORMATEAR
+# x = 10
+# y = 5
+
+# print("Mis numeros son " + str(x) + " y " + str(y))
+
+# print("Mis numeros son {} y {}".format(x,y))
+# print("Mis numeros son {} y {}".format(y,x))
+# print("La suma de {} y {} es igual a {}".format(x,y,x+y))
+
+# color = 'amarillo'
+# matricula = 654646
+
+# print(f"El auto es {color} y la matricula {matricula}")
+
+# nombre_asociado = "Coshy Loko"
+# numero_asociado = 327469
+# print(f"Estimad@ {nombre_asociado}, su numero de asociado es: {numero_asociado}")
+
+
+######LISTAS#########
+
+# mi_lista = ['a','b','c']
+# mi_lista2 = ['d','e','f']
+# mi_lista3 = mi_lista + mi_lista2
+# #eliminado = mi_lista3.pop(0)  MUESTRA EL ELEMENTO ELIMINADO
+# #mi_lista3.pop(0) ELIMINA EL ELEMENTO INDICADO
+# #mi_lista3.pop()   SE ELIMINA EL ULTIMO ELEMENTO 
+# #mi_lista3.append('g')  SE AGREGA UN NUEVO ELEMENTO
+# #mi_lista3[0] = 'alfa'  CAMBIAR ELEMENTOS DE UNA LISTA
+# #otra_lista = ['hola', 545, 6.58]   COMBINADAS
+# #resultado = len(mi_lista)  LONGITUD
+# #resultado = mi_lista[0] SE PUEDE EXTRAER VALORES INDEXAR
+# #resultado = mi_lista[0:2] MUESTRA VOLORES INDICADOS
+# #print(resultado)
+# #print(mi_lista + mi_lista2) SE PUEDEN CONCATENAR LISTAS DIFERENTES
+# print(mi_lista3)
+# #print(eliminado)    
+
+
+
+# # ######METODOS DE UNA LISTA
+
+# lista = ['g','o','b','m','c']
+# # lista = [12,4,7,3,9]
+# #lista.reverse() ORDENA DE ATRAS HACIA ADELANTE 
+# #lista.sort()  ORDENA LOS ELEMENTOS 
+# print(lista)
